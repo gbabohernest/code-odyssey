63f9b5e334abc1923a013e43c0dc4e9b4593f482
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-03-05 22:47:32 +0300

    Return student grade as a letter grade

---
+++ high_level_programming/fcc-dsa-js/gradeBook/getGrade.js
@@ -0,0 +1,23 @@
+/**
+ * Step 2
+ * Now the teacher needs your help converting the student score to a letter grade.
+ *
+ * Complete the getGrade function that takes a number score as a parameter. Your function
+ * should return a string representing a letter grade based on the score.
+ *
+ *   100 -> A++ | 90 - 99 ->  A | 80 - 89 ->  B | 70 - 79 ->  C |  60 - 69 ->  D | 0 - 59 ->   f
+ */
+
+const getGrade = (score) => {
+  if (score === 100) return 'A++';
+  if (score >= 90 && score <= 99) return 'A';
+  if (score >= 80 && score <= 89) return 'B';
+  if (score >= 70 && score <= 79) return 'C';
+  if (score >= 60 && score <= 69) return 'D';
+  if (score <= 59) return 'F';
+};
+
+//test
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
