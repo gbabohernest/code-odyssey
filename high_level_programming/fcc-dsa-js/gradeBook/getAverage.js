855eaae9bd48f108049dcd7a26f88424f114b537
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-03-05 21:59:19 +0300

    calculate and return the average score

---
+++ high_level_programming/fcc-dsa-js/gradeBook/getAverage.js
@@ -0,0 +1,23 @@
+/*
+Step 1:
+ * A teacher has finished grading their students' tests and needs your help to calculate the average score for the class.
+ *
+ * Complete the getAverage function which takes in an array of test scores and returns the average score.
+ *
+ * The average is calculated by adding up all the scores and dividing by the total number of scores.
+ *
+ * average = sum of all scores / total number of scores
+ */
+
+const getAverage = (scores) => {
+    let totalScore = 0;
+
+    for (const score of scores) {
+        totalScore += score;
+    }
+    return totalScore / scores.length;
+}
+
+// test
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
