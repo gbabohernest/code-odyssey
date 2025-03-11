b654f30367734d5cdd13c664a944e74b236eeebf
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-03-11 20:29:56 +0300

    fibo of 50

---
+++ low_level_programming/0x02-functions_nested_loops/102-fibionacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* prints the first 50 fibonacci numbers. starting with 1 and 2
+* :Return - 0 (success)
+*/
+
+int main(void) {
+  long int num1 = 0, num2 = 1, num3;
+  int num = 1;
+
+  while (num < 50) {
+    num3 = num1 + num2;
+    printf("%ld", num3);
+    if (num < 49) {
+      printf(", ");
+    }
+    num1 = num2;
+    num2 = num3;
+    num++;
+  }
+
+  printf("\n");
+  return (0);
+}
