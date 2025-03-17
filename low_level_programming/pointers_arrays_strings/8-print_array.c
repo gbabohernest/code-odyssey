48ab80519882e8b6d3963f1fe85f5166ac6cc133
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-03-17 17:41:15 +0300

    print n elements of an array

---
+++ low_level_programming/pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_array - prints n elements of an array of integers
+* @a : Address of array's first element.
+* @n : number of elements to be printed.
+*/
+
+void print_array(int *a, int n)
+{
+      int i = 0;
+      while (i < n) {
+        printf("%d", a[i]);
+        if (!(n - i == 1)) {
+          printf(", ");
+        }
+        i++;
+
+      }
+      printf("\n");
+}
