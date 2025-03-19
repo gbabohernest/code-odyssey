94af213c2aa5440fe629953b63c9e44e8aa8eba7
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-03-19 22:35:32 +0300

    concatenates two strings

--- low_level_programming/more_pointers_arrays_strings/1-strncat.c
+++ low_level_programming/more_pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 char * _strncat(char *dest, char *src, int n)
 {
 	int i = 0, count = 0;
@@ -9,18 +10,12 @@ char * _strncat(char *dest, char *src, int n)
 		i++;
 	}
 
-	if (n == 0)
-	{
-		dest[i] = src[n];
-		return dest;
-	}
-
-	while (count < n)
-	{
-		dest[i] = src[count];
-		i++;
-		count++;
-	}
+	while (src[count] != '\0' && count < n)
+    {
+          dest[i] = src[count];
+          i++;
+          count++;
+    }
 
 	return dest;
 
