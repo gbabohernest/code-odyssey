7d89dbac3301221f16c7ac639f58f8469ae5b50c
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-03-27 23:52:11 +0300

    tasks routes

--- high_level_programming/task-manager-api/routes/task.routes.js
+++ high_level_programming/task-manager-api/routes/task.routes.js
@@ -1,37 +1,22 @@
 import { Router } from 'express';
+import {
+  createTask,
+  getAllTask,
+  getSingleTask,
+  updateTask,
+  deleteTask,
+} from '../controllers/task-controller.js';
 const taskRouter = Router();
 
-/**
- ** /api/v1/tasks
- */
+taskRouter
+  .route('/')
+  .get(await getAllTask)
+  .post(await createTask);
 
-taskRouter.get('/', (req, res) => {
-  res.status(200).json({ success: true, message: 'GET all tasks' });
-});
-
-taskRouter.post('/', (req, res) => {
-  res
-    .status(201)
-    .json({ success: true, message: 'Created a tasks', data: req.body });
-});
-
-taskRouter.get('/:id', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Get a single task',
-  });
-});
-
-taskRouter.put('/:id', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Updated successfully',
-    data: req.body,
-  });
-});
-
-taskRouter.delete('/:id', (req, res) => {
-  res.status(200).json({ success: true, message: 'deleted successfully' });
-});
+taskRouter
+  .route('/:id')
+  .get(await getSingleTask)
+  .put(await updateTask)
+  .delete(await deleteTask);
 
 export default taskRouter;
