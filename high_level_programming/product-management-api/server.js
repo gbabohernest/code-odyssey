eeee9d7c99a8d27d610f6fd1916ed6f896bea584
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-03-29 16:55:23 +0300

    server.js

--- high_level_programming/product-management-api/server.js
+++ high_level_programming/product-management-api/server.js
@@ -1,15 +1,32 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import productRouter from "./routes/product.routes.js";
+import mongoDbConnection from "./database/mongodb-connection.js";
+import customerRouter from "./routes/customer-routes.js";
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/customers", customerRouter);
+
 app.get("/", (req, res) => {
-  res.send("hi there , route is working");
-  console.log(PORT);
+  res.status(200).json("Welcome to the Project Management API");
 });
 
-app.listen(PORT, () => {
-  console.log(
-    `Product Management API is now listening on http://localhost:${PORT}`,
-  );
-});
+const startSever = async () => {
+  try {
+    await mongoDbConnection();
+
+    app.listen(PORT, () => {
+      console.log(
+        `Product Management API is now listening on http://localhost:${PORT}`,
+      );
+    });
+  } catch (e) {
+    console.error(`Something went wrong, Cannot start the sever: ${e}`);
+  }
+};
+
+await startSever();
