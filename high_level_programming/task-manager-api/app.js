e14448018c67fb70cc0e05c55ae94604286f9b97
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-03-31 00:10:29 +0300

    add a custom error middleware to be use by the app; handles all routes which are not registered and provide a custom 404 message

--- high_level_programming/task-manager-api/app.js
+++ high_level_programming/task-manager-api/app.js
@@ -2,10 +2,13 @@ import express from "express";
 import { PORT } from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
 import taskRouter from "./routes/task.routes.js";
+import { errorMiddleware } from "./middlewares/error-middleware.js";
 
 const app = express();
 app.use(express.json());
 app.use("/api/v1/tasks", taskRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res
@@ -13,6 +16,11 @@ app.get("/", (req, res) => {
     .json({ success: true, message: "Welcome to the task-manager-api." });
 });
 
+app.all("*", (req, res) => {
+  res
+    .status(404)
+    .send(`<h2>Sorry, I do not know what you are looking for.  </h2>`);
+});
 const startSever = async () => {
   try {
     await connectToDatabase();
