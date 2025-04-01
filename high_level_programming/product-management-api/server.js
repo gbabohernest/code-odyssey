a85450cd3fcad7f38bf4f3c7c4f297948e7381f2
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-02 02:40:07 +0300

    custom error middleware

--- high_level_programming/product-management-api/server.js
+++ high_level_programming/product-management-api/server.js
@@ -3,6 +3,7 @@ import { PORT } from "./config/env.js";
 import productRouter from "./routes/product.routes.js";
 import mongoDbConnection from "./database/mongodb-connection.js";
 import customerRouter from "./routes/customer-routes.js";
+import { errorMiddleware } from "./middlewares/custom-error.middleware.js";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/customers", customerRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.status(200).json("Welcome to the Project Management API");
