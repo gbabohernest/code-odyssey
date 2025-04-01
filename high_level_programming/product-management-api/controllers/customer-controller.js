b56c9e3452751bb9f2ca9b63f00ec55e258916ac
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-01 15:12:26 +0300

    use a async middleware to handle repetitive try catch in handler function

--- high_level_programming/product-management-api/controllers/customer-controller.js
+++ high_level_programming/product-management-api/controllers/customer-controller.js
@@ -1,80 +1,60 @@
 import Customer from "../models/customer-model.js";
+import { asyncMiddleware } from "../middlewares/async-wrapper.middleware.js";
 
-const getCustomers = async (req, res) => {
-  try {
-    const customers = await Customer.find({});
-    if (!customers || customers.length === 0) {
-      return res
-        .status(404)
-        .json({ success: false, message: "No customer found" });
-    }
-    res.status(200).json({
-      success: true,
-      message: "all customer(s) retrieved",
-      data: customers,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: `Something went wrong: ${error.message}`,
-    });
+const getCustomers = asyncMiddleware(async (req, res) => {
+  const customers = await Customer.find({});
+  if (!customers || customers.length === 0) {
+    return res
+      .status(404)
+      .json({ success: false, message: "No customer found" });
   }
-};
+  res.status(200).json({
+    success: true,
+    message: "all customer(s) retrieved",
+    data: customers,
+  });
+});
 
-const createCustomer = async (req, res) => {
-  try {
-    const customer = await Customer.create(req.body);
-    res
-      .status(201)
-      .json({ success: true, message: "customer created", data: customer });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ success: false, message: `something went wrong: ${error}` });
+const createCustomer = asyncMiddleware(async (req, res) => {
+  const customer = await Customer.create(req.body);
+  res
+    .status(201)
+    .json({ success: true, message: "customer created", data: customer });
+});
+
+const getCustomer = asyncMiddleware(async (req, res) => {
+  const { id: customerID } = req.params;
+  const customer = await Customer.findById(customerID);
+
+  if (!customer) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Customer NOT Found" });
   }
-};
 
-const getCustomer = async (req, res) => {
-  try {
-    const { id: customerID } = req.params;
-    const customer = await Customer.findById(customerID);
+  res
+    .status(200)
+    .json({ success: true, message: "customer retrieved", data: customer });
+});
 
-    if (!customer) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Customer NOT Found" });
-    }
-
-    res
-      .status(200)
-      .json({ success: true, message: "customer retrieved", data: customer });
-  } catch (error) {
-    res.status(400).json({ success: false, message: `Bad Request : ${error}` });
+const updateCustomer = asyncMiddleware(async (req, res) => {
+  const { id: customerID } = req.params;
+  const customer = await Customer.findByIdAndUpdate(customerID, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!customer) {
+    return res
+      .status(404)
+      .json({ success: false, message: "customer NOT Found" });
   }
-};
 
-const updateCustomer = async (req, res) => {
-  try {
-    const { id: customerID } = req.params;
-    const customer = await Customer.findByIdAndUpdate(customerID, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!customer) {
-      return res
-        .status(404)
-        .json({ success: false, message: "customer NOT Found" });
-    }
+  res
+    .status(200)
+    .json({ success: true, message: "update success", data: customer });
+});
 
-    res
-      .status(200)
-      .json({ success: true, message: "update success", data: customer });
-  } catch (error) {
-    res.status(400).json({ success: false, message: `Bad request : ${error}` });
-  }
-};
-
-const deleteCustomer = async (req, res) => {
+const deleteCustomer = asyncMiddleware(async (req, res) => {
   try {
     const customer = await Customer.findByIdAndDelete(req.params.id);
     if (!customer) {
@@ -87,7 +67,7 @@ const deleteCustomer = async (req, res) => {
   } catch (error) {
     res.status(400).json({ success: false, message: `Bad Request : ${error}` });
   }
-};
+});
 
 export {
   getCustomers,
