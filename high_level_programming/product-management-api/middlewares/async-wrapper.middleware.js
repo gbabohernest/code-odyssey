bc388d9bedb18b94fbcfc98123bcac1f04d17d2f
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-01 15:38:54 +0300

    CustomAPI Error

--- high_level_programming/product-management-api/middlewares/async-wrapper.middleware.js
+++ high_level_programming/product-management-api/middlewares/async-wrapper.middleware.js
@@ -12,4 +12,24 @@ const asyncMiddleware = (handlerFn) => {
   };
 };
 
-export { asyncMiddleware };
+/**
+ * Handles custom Error
+ */
+class CustomAPIError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+/**
+ * Return a CustomAPIError instance.
+ * @param message - Error message
+ * @param status - Error Code
+ * @returns {CustomAPIError} - An instance of CustomAPIError
+ */
+const customError = (message, status) => {
+  return new CustomAPIError(message.status);
+};
+
+export { asyncMiddleware, customError, CustomAPIError };
