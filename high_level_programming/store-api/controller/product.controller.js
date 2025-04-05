668bfe2cd5c365e6cfd255ab651df3c3c6710098
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-05 18:28:52 +0300

    filter data base on different filter parameters

--- high_level_programming/store-api/controller/product.controller.js
+++ high_level_programming/store-api/controller/product.controller.js
@@ -3,15 +3,38 @@ const Product = require("../models/product.model");
 const asyncWrapper = require("../middlewares/async-wrapper.middleware");
 
 const getProducts = asyncWrapper(async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort, field } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true";
   }
 
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
   console.log(queryObject);
+
   let query = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    query = query.sort(sortList);
+  } else {
+    query.sort({ name: 1 });
+  }
+
+  if (field) {
+    const filedList = field.split(",").join("");
+    query = query.select(filedList);
+  } else {
+    query = query.select({ name: 1, price: 1, currency: 1, rating: 1 });
+  }
 
   const products = await query;
 
