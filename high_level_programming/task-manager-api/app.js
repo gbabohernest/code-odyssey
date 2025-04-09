18c950de711d56eb4ef48d73f5f788bbce0fc647
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-09 23:53:05 +0300

    import express-async-error package

--- high_level_programming/task-manager-api/app.js
+++ high_level_programming/task-manager-api/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import "express-async-errors";
 import connectToDatabase from "./database/mongodb.js";
 import taskRouter from "./routes/task.routes.js";
 import { errorMiddleware } from "./middlewares/error-middleware.js";
@@ -7,8 +8,6 @@ import { errorMiddleware } from "./middlewares/error-middleware.js";
 const app = express();
 app.use(express.json());
 app.use("/api/v1/tasks", taskRouter);
-
-app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res
@@ -21,11 +20,15 @@ app.all("*", (req, res) => {
     .status(404)
     .send(`<h2>Sorry, I do not know what you are looking for.  </h2>`);
 });
+
+app.use(errorMiddleware);
+
+const port = PORT || 3000;
 const startSever = async () => {
   try {
     await connectToDatabase();
-    app.listen(PORT, () => {
-      console.log(`Task Manager Api is running on http://localhost:${PORT}`);
+    app.listen(port, () => {
+      console.log(`Task Manager Api is running on http://localhost:${port}`);
     });
   } catch (error) {
     console.error(error.message);
