e3fb66fba72dd0ceb7d59c3111cfea0b7f3b238e
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-11 23:05:32 +0300

    remove the calls to the asyncMiddleware function to reduce the noise in the code base, use express-async-errors to handle unhandled promise rejection

--- high_level_programming/product-management-api/controllers/customer-controller.js
+++ high_level_programming/product-management-api/controllers/customer-controller.js
@@ -1,8 +1,8 @@
 import Customer from "../models/customer-model.js";
-import { asyncMiddleware } from "../middlewares/async-wrapper.middleware.js";
+// import { asyncMiddleware } from "../middlewares/async-wrapper.middleware.js";
 import { customError } from "../errors/api-error.js";
 
-const getCustomers = asyncMiddleware(async (req, res) => {
+const getCustomers = async (req, res) => {
   const customers = await Customer.find({});
   if (!customers || customers.length === 0) {
     throw customError("No customer found!", 404);
@@ -12,16 +12,16 @@ const getCustomers = asyncMiddleware(async (req, res) => {
     message: "all customer(s) retrieved",
     data: customers,
   });
-});
+};
 
-const createCustomer = asyncMiddleware(async (req, res, next) => {
+const createCustomer = async (req, res) => {
   const customer = await Customer.create(req.body);
   res
     .status(201)
     .json({ success: true, message: "customer created", data: customer });
-});
+};
 
-const getCustomer = asyncMiddleware(async (req, res) => {
+const getCustomer = async (req, res) => {
   const { id: customerID } = req.params;
   const customer = await Customer.findById(customerID);
 
@@ -32,9 +32,9 @@ const getCustomer = asyncMiddleware(async (req, res) => {
   res
     .status(200)
     .json({ success: true, message: "customer retrieved", data: customer });
-});
+};
 
-const updateCustomer = asyncMiddleware(async (req, res) => {
+const updateCustomer = async (req, res) => {
   const { id: customerID } = req.params;
   const customer = await Customer.findByIdAndUpdate(customerID, req.body, {
     new: true,
@@ -47,16 +47,17 @@ const updateCustomer = asyncMiddleware(async (req, res) => {
   res
     .status(200)
     .json({ success: true, message: "update success", data: customer });
-});
+};
 
-const deleteCustomer = asyncMiddleware(async (req, res) => {
+const deleteCustomer = async (req, res) => {
   const customer = await Customer.findByIdAndDelete(req.params.id);
   if (!customer) {
     throw customError("Customer NOT Found!", 404);
   }
 
-  res.status(204);
-});
+  // res.status(204);
+  res.status(200).json({ success: true, message: "customer deleted" });
+};
 
 export {
   getCustomers,
