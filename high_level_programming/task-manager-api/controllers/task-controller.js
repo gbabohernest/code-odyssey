fc8da8d07ee3c308f4c989e265dbd1316ed4d224
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-13 04:10:20 +0300

    Refactor, use custom NotFoundError class to handle not found errors in route handler

--- high_level_programming/task-manager-api/controllers/task-controller.js
+++ high_level_programming/task-manager-api/controllers/task-controller.js
@@ -1,4 +1,5 @@
-import Task from "../models/tasks-model.js";
+import { NotFoundError } from '../errors/index.error.js';
+import Task from '../models/tasks-model.js';
 // import { asyncWrapper } from "../middlewares/async-middleware.js";
 
 const getAllTask = async (req, res) => {
@@ -10,7 +11,7 @@ const createTask = async (req, res) => {
   const task = await Task.create(req.body);
   res.status(201).json({
     success: true,
-    message: "task created successfully",
+    message: 'task created successfully',
     data: task,
   });
 };
@@ -18,11 +19,9 @@ const createTask = async (req, res) => {
 const getSingleTask = async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
-  if (!task) {
-    const error = new Error("Task Not Found");
-    error.statusCode = 404;
-    throw error;
-  }
+
+  if (!task) throw new NotFoundError('Task Not Found!');
+
   res.status(200).json({ success: true, data: task });
 };
 
@@ -32,26 +31,18 @@ const updateTask = async (req, res) => {
     new: true,
     runValidators: true,
   });
-  if (!task) {
-    const error = new Error("Task Not Found");
-    error.statusCode = 404;
-    throw error;
-  }
+  if (!task) throw new NotFoundError('Task Not Found!');
 
   res
     .status(200)
-    .json({ success: true, message: "update success", data: task });
+    .json({ success: true, message: 'update success', data: task });
 };
 
 const deleteTask = async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
 
-  if (!task) {
-    const error = new Error("No Task Found");
-    error.statusCode = 404;
-    throw error;
-  }
+  if (!task) throw new NotFoundError('No Task Found');
   res.status(204).end();
 };
 
