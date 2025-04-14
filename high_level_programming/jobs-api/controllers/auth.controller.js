465e5e10060d18bc4a4c5bb670cb4dda6f4d6dc4
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-15 01:09:41 +0300

    Register user, hashed user password using bcrypt,  and generate token using jwt

--- high_level_programming/jobs-api/controllers/auth.controller.js
+++ high_level_programming/jobs-api/controllers/auth.controller.js
@@ -1,9 +1,51 @@
 import { StatusCodes } from "http-status-codes";
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import { CustomAPIError } from "../utils/index.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 
-const signup = async (req, res) => {
-  res
-    .status(StatusCodes.CREATED)
-    .json({ success: true, message: "user created" });
+const signup = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  try {
+    await session.startTransaction();
+    const { name, email, password } = req.body;
+
+    // check if email exists
+    const existingUser = User.findOne({ email })
+      .select({ email: 1 })
+      .session(session);
+    if (existingUser) {
+      throw new CustomAPIError("User already exists", StatusCodes.CONFLICT);
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create([{ name, email, password: password }], {
+      session,
+    });
+
+    const token = await jwt.sign(
+      { name, email, password: password },
+      JWT_SECRET,
+      {
+        expiresIn: JWT_EXPIRES_IN,
+      },
+    );
+    await session.commitTransaction();
+
+    res
+      .status(StatusCodes.CREATED)
+      .json({ success: true, message: "user created" });
+  } catch (error) {
+    await session.abortTransaction();
+    next(error);
+  } finally {
+    await session.endSession();
+  }
 };
 
 const login = async (req, res) => {
