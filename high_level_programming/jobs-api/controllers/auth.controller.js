378f734119f6e41fd96db27dfb5400c10f9079bf
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-18 21:13:36 +0300

    verify user provided password with hashed pasword; provide useful feedbacks for better debugging

--- high_level_programming/jobs-api/controllers/auth.controller.js
+++ high_level_programming/jobs-api/controllers/auth.controller.js
@@ -1,11 +1,11 @@
-import { StatusCodes } from "http-status-codes";
-import mongoose from "mongoose";
-import User from "../models/user.model.js";
+import { StatusCodes } from 'http-status-codes';
+import mongoose from 'mongoose';
+import User from '../models/user.model.js';
 import {
   BadRequestError,
   CustomAPIError,
   UnauthenticatedError,
-} from "../utils/index.js";
+} from '../utils/index.js';
 
 const signup = async (req, res, next) => {
   const session = await mongoose.startSession();
@@ -14,24 +14,22 @@ const signup = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     // check if email exists
-    const existingUser = await User.findOne({ email })
-      .select({ email: 1 })
-      .session(session);
+    const existingUser = await User.findOne({ email }, { email: 1 }, null);
 
     if (existingUser) {
-      throw new CustomAPIError("User already exists", StatusCodes.CONFLICT);
+      throw new CustomAPIError('User already exists', StatusCodes.CONFLICT);
     }
 
     const user = await User.create([{ name, email, password }], {
       session,
     });
 
-    const token = user[0].createJWT();
+    const token = await user[0].createJWT();
     await session.commitTransaction();
 
     res.status(StatusCodes.CREATED).json({
       success: true,
-      message: "user created",
+      message: 'user created',
       user: user[0].name,
       token,
     });
@@ -45,21 +43,28 @@ const signup = async (req, res, next) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password)
-    throw new BadRequestError("Email and Password are required");
 
-  const user = await User.findOne({ email }, "", null);
-  if (!user) throw new UnauthenticatedError("Invalid Credentials");
+  if (!email || !password) {
+    throw new BadRequestError('Email and Password are required');
+  }
+
+  const user = await User.findOne(
+    { email },
+    { email: 1, name: 1, password: 1 },
+    null
+  );
+
+  if (!user) throw new UnauthenticatedError('Invalid Email ID');
 
   const isPasswordValid = await user.compareAndVerifyPassword(password);
 
-  if (!isPasswordValid) throw new UnauthenticatedError("Invalid Credentials");
+  if (!isPasswordValid) throw new UnauthenticatedError('Invalid Password');
 
-  const token = user.createJWT();
+  const token = await user.createJWT();
 
   res
     .status(StatusCodes.OK)
-    .json({ success: true, message: "login success", user: user.name, token });
+    .json({ success: true, message: 'login success', user: user.name, token });
 };
 
 export { signup, login };
