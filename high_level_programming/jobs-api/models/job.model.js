c86804d99426e6c87cb182ce28cdda00f1918bac
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-19 02:41:13 +0300

    Add unique compound index to prevent a user from creating duplicate job(s) for same company; Pre save hook for user-friendly error messages

--- high_level_programming/jobs-api/models/job.model.js
+++ high_level_programming/jobs-api/models/job.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { BadRequestError } from "../utils/index.js";
 
 const jobSchema = new mongoose.Schema(
   {
@@ -34,9 +35,40 @@ const jobSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
+//unique compound index [these three fields must always be unique]
+jobSchema.index({ company: 1, position: 1, createdBy: 1 }, { unique: true });
+
 jobSchema.pre("save", function (next) {
   this.company = this.company.toUpperCase();
   this.position = this.position.toUpperCase();
+  next();
+});
+
+/**
+ * Restrict user from creating duplicate job(s) for the same company.
+ * Pre 'save' hook to provide user-friendly error message.
+ */
+jobSchema.pre("save", async function (next) {
+  // check if the doc is new
+  if (this.isNew) {
+    const existingPosition = await mongoose.models.Job.findOne(
+      {
+        company: this.company,
+        position: this.position,
+        createdBy: this.createdBy,
+      },
+      { company: 1, position: 1, createdBy: 1 },
+      null,
+    );
+    if (existingPosition) {
+      return next(
+        new BadRequestError(
+          "Job with this name and for this company already created by this User",
+        ),
+      );
+    }
+  }
+
   next();
 });
 
