08c66e2cf24b90869768b2b6f305735e402f71a1
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-18 23:18:33 +0300

    authentication middleware, protects routes that should be accessible by authenticated user only

---
+++ high_level_programming/jobs-api/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+// authentication middleware,protects the job routes
+
+import { UnauthenticatedError } from "../utils/index.js";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
+
+const authMiddleware = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("Unauthorized Access");
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const payload = await jwt.verify(token, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    req.user = { userID: payload.userID, name: payload.name };
+
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError(error.message);
+  }
+};
+
+export { authMiddleware };
