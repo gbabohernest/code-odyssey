9acc66bd651f0c09b757b546afffd2ed94575d57
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-18 23:57:23 +0300

    Add mongoose validation error

--- high_level_programming/jobs-api/middlewares/error.middleware.js
+++ high_level_programming/jobs-api/middlewares/error.middleware.js
@@ -1,10 +1,18 @@
 import { StatusCodes, getReasonPhrase } from "http-status-codes";
 
 const { INTERNAL_SERVER_ERROR, BAD_REQUEST } = StatusCodes;
+
 const errorMiddleware = (err, req, res, next) => {
   let message = err.message || getReasonPhrase(INTERNAL_SERVER_ERROR);
   let statusCode = err.statusCode || INTERNAL_SERVER_ERROR;
 
+  //mongoose validation error
+  if (err.name === "ValidationError") {
+    if (err.errors) {
+      const msg = Object.values(err.errors);
+      message = msg.map((value) => value.message).join(", ");
+    }
+  }
   res.status(statusCode).json({ success: false, message });
 };
 
