4b9cbbeef06b41e59a60a4c31cd3dd2a6ed5250a
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-20 03:03:07 +0300

    check for mongoose duplicate key error & bad objectID error and handle them gracefully

--- high_level_programming/jobs-api/middlewares/error.middleware.js
+++ high_level_programming/jobs-api/middlewares/error.middleware.js
@@ -6,11 +6,23 @@ const errorMiddleware = (err, req, res, next) => {
   let message = err.message || getReasonPhrase(INTERNAL_SERVER_ERROR);
   let statusCode = err.statusCode || INTERNAL_SERVER_ERROR;
 
-  //mongoose validation error
+  //Mongoose duplicate key error
+  if (err.code === 11000) {
+    message = "Duplicate Key Error Collection [ value(s) already exists ]";
+    statusCode = BAD_REQUEST;
+  }
+
+  // Mongoose bad ObjectID | Cast Error
+  if (err.name === "CastError") {
+    message = "Bad Object ID.";
+    statusCode = BAD_REQUEST;
+  }
   if (err.name === "ValidationError") {
+    //mongoose validation error
     if (err.errors) {
       const msg = Object.values(err.errors);
       message = msg.map((value) => value.message).join(", ");
+      statusCode = BAD_REQUEST;
     }
   }
   res.status(statusCode).json({ success: false, message });
