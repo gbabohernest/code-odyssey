bddd89691614651dbaba69d036ccee50b382fedb
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-22 11:23:53 +0300

    Jobs API

--- high_level_programming/jobs-api/app.js
+++ high_level_programming/jobs-api/app.js
@@ -4,12 +4,13 @@ import { PORT } from "./config/env.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import authRouter from "./routes/auth.route.js";
 import jobRouter from "./routes/jobs.route.js";
+import { authMiddleware } from "./middlewares/auth.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authMiddleware, jobRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({
