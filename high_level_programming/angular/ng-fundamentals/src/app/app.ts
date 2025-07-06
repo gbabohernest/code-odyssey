62d413be10360e1f8c647cb20c493bb5a5dce58d
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-07-06 19:02:35 +0300

    pipes basic - built-in pipes

--- high_level_programming/angular/ng-fundamentals/src/app/app.ts
+++ high_level_programming/angular/ng-fundamentals/src/app/app.ts
@@ -3,10 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { DataBinding } from './data-binding/data-binding';
 import { Directives } from './directives/directives';
 import { Courses } from './services/courses';
+import { CurrencyPipe, DecimalPipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DataBinding, Directives],
+  imports: [
+    RouterOutlet,
+    DataBinding,
+    Directives,
+    CurrencyPipe,
+    UpperCasePipe,
+    DecimalPipe,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
@@ -14,6 +22,10 @@ export class App {
   protected title = 'Ng Fundamentals';
 
   courses;
+  price = 308539;
+  numOfStudents = 5024;
+  rating = 4.821;
+  pipeTitle = 'testing pipe';
 
   constructor(private course: Courses) {
     this.courses = course.getCourses();
