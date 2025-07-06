3afd4a8d4b59e5222d65928e713229a5ddedb4de
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-07-06 18:43:48 +0300

    consuming services

--- high_level_programming/angular/ng-fundamentals/src/app/app.ts
+++ high_level_programming/angular/ng-fundamentals/src/app/app.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {DataBinding} from './data-binding/data-binding';
-import {Directives} from './directives/directives';
+import { DataBinding } from './data-binding/data-binding';
+import { Directives } from './directives/directives';
+import { Courses } from './services/courses';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, DataBinding, Directives],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'Ng Fundamentals';
 
+  courses;
 
+  constructor(private course: Courses) {
+    this.courses = course.getCourses();
+  }
 }
