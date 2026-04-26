4e8d6b0e53f0e6a4cbd372b7d726c7696a554805
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2026-04-26 17:36:36 +0300

    docs: set up swagger documentation for API

--- high_level_programming/project-tracker-api/src/main.ts
+++ high_level_programming/project-tracker-api/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { cleanupOpenApiDoc } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //setup swagger docs for our API
+  const config = new DocumentBuilder()
+    .setTitle('Project-Tracker-API')
+    .setDescription('Demonstrating Nest Js + Zod + TypeORM Dev')
+    .setVersion('1.0')
+    .build();
+
+  const rawDocument = SwaggerModule.createDocument(app, config);
+  const document = cleanupOpenApiDoc(rawDocument);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
