d228ac1ad5748ecc7f1ae86392afce2c59c35ec3
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2026-04-26 17:38:41 +0300

    feat: add global Zod validation

--- high_level_programming/project-tracker-api/src/app.module.ts
+++ high_level_programming/project-tracker-api/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ZodSerializationException, ZodValidationPipe } from 'nestjs-zod';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ZodSerializationException,
+    },
+  ],
 })
 export class AppModule {}
