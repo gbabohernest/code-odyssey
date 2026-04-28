4a5821eafb21dd84e87a914211538025af9e91d6
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2026-04-28 23:57:25 +0300

    chore: add db module

--- high_level_programming/project-tracker-api/src/app.module.ts
+++ high_level_programming/project-tracker-api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { ZodSerializationException, ZodValidationPipe } from 'nestjs-zod';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [DatabaseModule, UsersModule],
   providers: [
     {
       provide: APP_PIPE,
