b21b1a8a64ca89d35c3eb816107764d64212450c
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2026-05-02 15:28:47 +0300

    feat: user crud endpoints, data persistance using mysql & typeORM'

--- high_level_programming/project-tracker-api/src/users/users.controller.ts
+++ high_level_programming/project-tracker-api/src/users/users.controller.ts
@@ -10,11 +10,10 @@ import {
   Post,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UserResponseDto } from './dto/user-response.dto';
-import { ZodResponse } from 'nestjs-zod';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user-dto';
+import { Users } from './entities/users.entities';
 
 @Controller('users')
 @ApiTags('Users')
@@ -22,40 +21,36 @@ export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get()
-  @ZodResponse({ type: [UserResponseDto] })
   @ApiOperation({ summary: 'Fetch all users' })
-  findAll(): UserResponseDto[] {
+  async findAll(): Promise<Users[]> {
     return this.userService.findAll();
   }
 
   @Get(':id')
-  @ZodResponse({ type: UserResponseDto })
   @ApiOperation({ summary: 'Fetch a user by ID' })
-  getUser(@Param('id') id: string): UserResponseDto {
-    return this.userService.findOne(id);
+  async getUser(@Param('id') id: string): Promise<Users> {
+    return await this.userService.findOne(id);
   }
 
   @Post()
-  @ZodResponse({ type: UserResponseDto })
   @ApiOperation({ summary: 'Create a user' })
-  create(@Body() payload: CreateUserDto): UserResponseDto {
-    return this.userService.create(payload);
+  async create(@Body() payload: CreateUserDto): Promise<Users> {
+    return await this.userService.create(payload);
   }
 
   @Patch(':id')
-  @ZodResponse({ type: UserResponseDto })
   @ApiOperation({ summary: 'Update a user by ID' })
-  update(
+  async update(
     @Param('id') id: string,
     @Body() payload: UpdateUserDto,
-  ): UserResponseDto {
-    return this.update(id, payload);
+  ): Promise<Users> {
+    return this.userService.update(id, payload);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({ summary: 'Delete a user by ID' })
-  delete(@Param('id') id: string): void {
-    this.userService.remove(id);
+  async delete(@Param('id') id: string): Promise<void> {
+    await this.userService.remove(id);
   }
 }
